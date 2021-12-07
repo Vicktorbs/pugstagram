495f9d4ff37d03312dbd7cbe402d099e487fbf96
Victor Bolaños Solís <viktorb.132@gmail.com>
2021-12-07 13:35:30 -0600

    Componentes estilizados

---
+++ src/components/Profile.svelte
@@ -0,0 +1,36 @@
+<style>
+    .Profile-content {
+        display: flex;
+        align-items: center;
+    }
+    .Profile-avatar img {
+        width: 42px;
+        height: 42px;
+        border-radius: 50%;
+    }
+    .Profile-info {
+        margin: 0 0 0 0.5em;
+    }
+    .Profile-info h2 {
+        font-size: 14px;
+        color: black;
+        margin: 0;
+        padding: 0;
+    }
+    .Profile-info span {
+        font-size: 12px;
+        font-weight: normal;
+    }
+</style>  
+
+<div class="Profile">
+    <div class="Profile-content">
+        <div class="Profile-avatar">
+            <img src="https://scontent.fmex18-1.fna.fbcdn.net/v/t1.6435-9/131640318_1743620212463980_7669679555384342514_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=vP4ngVdlhvEAX_3qqKL&_nc_ht=scontent.fmex18-1.fna&oh=2c596e2900c636897e0dc2d4b8606249&oe=61D59C54" alt="">
+        </div>
+        <div class="Profile-info">
+            <h2>Viktorbs</h2>
+            <span>Víctor Bolaños Solís</span>
+        </div>
+    </div>
+</div>
---
+++ src/components/Stories.svelte
@@ -0,0 +1,79 @@
+<style>
+    .Stories {
+        border: 1px solid rgba(219, 219, 219, 1);
+        border-radius: 4px;
+        margin: 1em 0;
+        padding: 0.5em 1em;
+        background-color: white;
+        width: 100%;
+    }
+    .Stories-item img {
+        width: 32px;
+        height: 32px;
+        border-radius: 50%;
+    }
+    .Stories-item {
+        display: flex;
+        align-items: center;
+        margin: 0 0 0.5em 0;
+    }
+    .Stories-item h2 {
+        font-size: 14px;
+        color: black;
+        margin: 0 0 0 0.5em;
+    }
+    .Stories-item h2 span {
+        display: block;
+        font-size: 9px;
+        text-transform: uppercase;
+        color: gray;
+    }
+    .Stories-head {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0.5em 0 0 0;
+    }
+    .Stories-head h2 {
+        font-size: 12px;
+        font-weight: 600;
+    }
+    .Stories-head span {
+        font-size: 12px;
+        font-weight: normal;
+        color: black;
+    }
+    .Stories-items {
+        margin: 0.5em 0 0.5em 0;
+    }
+    .Stories-item-box {
+        width: 32px;
+        height: 32px;
+        border: double 2px transparent;
+        border-radius: 100%;
+        background-image: linear-gradient(#fff, #fff),
+        radial-gradient(circle at top left, #f09433, #bc1888);
+        background-origin: border-box;
+        background-clip: content-box, border-box;
+    }
+</style>  
+
+<div class="Stories">
+    <div class="Stories-container">
+        <div class="Stories-head">
+            <h2>Historias</h2>
+            <span>Ver todas</span>
+        </div>
+        <div class="Stories-items">
+            <div class="Stories-item">
+                <div class="Stories-item-box">
+                    <img src="https://scontent.fmex18-1.fna.fbcdn.net/v/t1.6435-9/131640318_1743620212463980_7669679555384342514_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=vP4ngVdlhvEAX_3qqKL&_nc_ht=scontent.fmex18-1.fna&oh=2c596e2900c636897e0dc2d4b8606249&oe=61D59C54" alt="">
+                </div>
+                <h2>
+                    Viktorbs
+                    <span>10 horas antes</span>
+                </h2>
+            </div>
+        </div>
+    </div>
+</div>
